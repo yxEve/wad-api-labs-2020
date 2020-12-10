80e56e7641c1d6260b5393e06b6968c73f32cf17
Yuxin Nie <1371454048@qq.com>
2020-12-10 03:13:47 +0000

    Week 9 User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -24,4 +24,29 @@ router.put('/:id',  (req, res, next) => {
     })
     .then(user => res.json(200, user)).catch(next);
 });
+
+router.post('/:userName/favourites', (req, res, next) => {
+    const newFavourite = req.body;
+    const query = {username: req.params.userName};
+    if (newFavourite && newFavourite.id) {
+      User.find(query).then(
+        user => {
+          (user.favourites)?user.favourites.push(newFavourite):user.favourites =[newFavourite];
+          User.findOneAndUpdate(query, {favourites:user.favourites}, {
+            new: true
+          }).then(user => res.status(201).send(user));
+        }
+      ).catch(next);
+    } else {
+        res.status(401).send("Unable to find user")
+    }
+  });
+
+router.get('/:userName/favourites', (req, res, next) => {
+    const user = req.params.userName;
+    User.find( {username: user}).then(
+        user => res.status(201).send(user.favourites)
+    ).catch(next);
+  });
+
 export default router;
